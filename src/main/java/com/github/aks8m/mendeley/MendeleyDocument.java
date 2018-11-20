b5245518b6437acc1438246073c2d8637e621c9d
aks8m <andrew.k.sills@gmail.com>
2018-11-20 02:43:26 -0500

    Working oAuth 2.0 authentication to Mendeley, and subsequent Get calls for Documents

---
+++ src/main/java/com/github/aks8m/mendeley/MendeleyDocument.java
@@ -0,0 +1,12 @@
+package com.github.aks8m.mendeley;
+
+public class MendeleyDocument {
+
+    private final String id;
+    private final String title;
+
+    public MendeleyDocument(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+}
